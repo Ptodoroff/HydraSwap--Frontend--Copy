dd5903366a8de60c1b7ff38cca20209260a71c40
Petar <p.todorov212@gmail.com>
2022-10-23 21:25:45 +0300

    added walletconnect successfully

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,20 @@ import Main from "./components/Main";
 import { useEffect, useState } from "react";
 import { Web3OnboardProvider, init } from "@web3-onboard/react";
 import injectedModule from "@web3-onboard/injected-wallets";
+import walletConnectModule from "@web3-onboard/walletconnect";
 import { ethers } from "ethers";
 
+window.Buffer = window.Buffer || require("buffer").Buffer;
 function App() {
   const GOERLI_RPC_URL = `https://goerli.infura.io/v3/d92c482888c64718a93cfbc3082b73be`; /// iMPORTANT - SHOULD FIX IT PROCE.ENV.REACT_APP_GOERLI_KEY WONT WORK
 
   const injected = injectedModule();
+  const walletConnect = walletConnectModule({
+    bridge: "https://bridge.walletconnect.org",
+    qrcodeModalOptions: {
+      mobileLinks: ["metamask"],
+    },
+  });
 
   const ethereumGoerli = {
     id: "0x5",
@@ -18,7 +26,7 @@ function App() {
     rpcUrl: GOERLI_RPC_URL,
   };
   const chains = [ethereumGoerli];
-  const wallets = [injected];
+  const wallets = [injected, walletConnect];
 
   const web3Onboard = init({
     wallets,
